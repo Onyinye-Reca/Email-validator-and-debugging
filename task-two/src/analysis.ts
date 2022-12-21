3455df5bb533c17691b0565d268523b6d071db76
Onyinye-Reca <rebeccaobiajuru@gmail.com>
2022-12-21 15:38:30 +0100

    email validator

--- task-two/src/analysis.ts
+++ task-two/src/analysis.ts
@@ -4,8 +4,72 @@
  * @param {string[]} inputPaths An array of csv files to read
  * @param {string} outputPath The path to output the analysis
  */
-function analyseFiles(inputPaths: string[], outputPath: string) {
-  console.log('Complete the implementation in src/analysis.ts');
+const fs = require('fs');
+const validator = require('email-validator')
+
+function analyseFiles(inputPaths: string [], outputPath: string) {
+  let validEmails: string [] = [];
+  let validDomain: string [] = [];
+  let validDomainObj: {[key: string]: number} = {};
+  let validEmailsObj: {[key: string]: number} = {};
+  let totalValidEmails = 0;
+  let chunk = "";
+
+  const streamsData = fs.createReadStream(inputPaths[0])
+  const writeData = fs.createWriteStream(outputPath)
+
+  streamsData.on('data', (data: string) =>{
+    chunk += data
+  })
+
+  streamsData.on('end', () => {
+    let processedEmails = chunk.split('\n')
+
+    processedEmails.forEach(value => {
+      if(validator.validate(value)){
+        validEmails.push(value)
+
+        let validEmailDomain = value.split('@')[1];
+        validDomain.push(validEmailDomain)
+      }
+      if(value !== "" && value !== 'Emails'){
+        totalValidEmails++
+      }
+    })
+
+    for(let domains of validDomain){
+      if(validDomainObj[domains]) {
+       validDomainObj[domains] += 1;
+      }else{
+        validDomainObj[domains] = 1
+      }
+    }
+
+    for(let emails of validEmails){
+      let castEmail = String(emails)
+      if(validEmailsObj[castEmail]) {
+        validEmailsObj[castEmail] += 1;
+        }else{
+        validEmailsObj[castEmail] = 1
+        }
+    }
+
+    let outputObj = {
+      "valid-domains": Object.keys( validDomainObj),
+      "totalEmailsParsed": totalValidEmails,
+      "totalValidEmails": validEmails.length,
+      "categories": validDomainObj
+    }
+
+    const result = JSON.stringify(outputObj, null, " ")
+    writeData.write(result)
+    writeData.end()
+
+    writeData.on('finish', () => {
+      console.log('finished writing file')
+    })
+  })
 }
 
+analyseFiles(['./fixtures/inputs/small-sample.csv'] , 'report-analysis.json')
 export default analyseFiles;
--- task-two/src/validation.ts
+++ task-two/src/validation.ts
@@ -4,8 +4,40 @@
  * @param {string[]} inputPath An array of csv files to read
  * @param {string} outputFile The path where to output the report
  */
-function validateEmailAddresses(inputPath: string[], outputFile: string) {
-  console.log('Complete the implementation in src/validation.ts');
-}
 
+ const fs = require('fs');
+ const validator = require('email-validator')
+
+
+function validateEmailAddresses(inputPath: string[], outputFile: string) {
+  let validEmails: string [] = ['Emails'];
+  let chunk = "";
+
+  const streamsData = fs.createReadStream(inputPath[0])
+  const writeData = fs.createWriteStream(outputFile)
+
+  streamsData.on('data', (data: string) =>{
+    chunk += data
+  })
+
+  streamsData.on('end', () => {
+    let processedEmails = chunk.split('\n')
+
+    processedEmails.forEach(value => {
+      if(validator.validate(value)){
+        validEmails.push(value)
+      }
+    })
+
+    const newEmail = validEmails.join('\n')
+
+    writeData.write(newEmail)
+    writeData.end()
+
+    writeData.on('finish', () => {
+      console.log('finished writing file')
+    })
+})
+}
+//console.log(validateEmailAddresses(['/Users/decagon/ Task 4/week-4-task-Onyinye-Reca/task-two/fixtures/inputs/medium-sample.csv'] , 'medium-report-validation.csv'))
 export default validateEmailAddresses;
