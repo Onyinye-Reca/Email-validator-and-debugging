9b0148dbb471885335ae7213e9770f4328e6db80
Onyinye-Reca <rebeccaobiajuru@gmail.com>
2022-12-21 15:49:25 +0100

    commit

--- task-one/src/app.js
+++ task-one/src/app.js
@@ -1,13 +1,15 @@
 import FileTree from './fileTree';
-
 export function createFileTree(input) {
   const fileTree = new FileTree();
 
+    input.sort((a,b)=> a.id-b.id)
+    input[0].parentId = null;
+
   for (const inputNode of input) {
+
     const parentNode = inputNode.parentId
       ? fileTree.findNodeById(inputNode.parentId)
       : null;
-
     fileTree.createNode(
       inputNode.id,
       inputNode.name,
